102dc73856064cd7530f82d803535529784ffc71
kablondino <kablondino@gmail.com>
2019-10-29 14:59:47 +0100

    More powerline characters to play with

--- shell/prompt.zsh
+++ shell/prompt.zsh
@@ -17,23 +17,26 @@ setopt PROMPT_SUBST
 
 # Check for NerdFonts
 if fc-list | grep -i "nerd" > /dev/null; then
-	nerd_chars=("" "" "" "")
+	nerd_chars=("" "" "" "" "" "")
+#	nerd_chars=("" "" "" "" "" "")
+#	nerd_chars=("" "" "" "" "" "")
+#	nerd_chars=("" "" "" "" "" "")
 else
-	nerd_chars=("" "" "" "")
+	nerd_chars=("" "" "" "" "" "")
 fi
 
 # Load gcloud's project prompt
 . ~/dot_files/shell/zsh-gcloud-prompt/gcloud.zsh
 
 # Landscape colors
-vim_cmd_mode="%B%F{015}%K{012}${nerd_chars[1]}%F{012}%K{015} NORMAL %k%F{015}${nerd_chars[2]}%b%k%f"
-vim_ins_mode="%B%F{015}%K{022}${nerd_chars[1]}%F{022}%K{015} INSERT %k%F{015}${nerd_chars[2]}%b%k%f"
-vim_vis_mode="%B%F{057}%K{015}${nerd_chars[1]}%F{015}%K{057} VISUAL %k%F{015}${nerd_chars[2]}%b%k%f"
+vim_cmd_mode="%B%F{015}%K{012}${nerd_chars[1]}%F{012}%K{015} NORMAL %k%F{015}${nerd_chars[3]}%b%k%f"
+vim_ins_mode="%B%F{015}%K{022}${nerd_chars[1]}%F{022}%K{015} INSERT %k%F{015}${nerd_chars[3]}%b%k%f"
+vim_vis_mode="%B%F{057}%K{015}${nerd_chars[1]}%F{015}%K{057} VISUAL %k%F{015}${nerd_chars[3]}%b%k%f"
 
 # Deus colors
-vim_cmd_mode="%B%F{114}${nerd_chars[1]}%F{232}%K{114} NORMAL %k%F{114}${nerd_chars[2]}%b%f"
-vim_ins_mode="%B%F{039}${nerd_chars[1]}%F{232}%K{039} INSERT %k%F{039}${nerd_chars[2]}%b%f"
-vim_vis_mode="%B%F{232}${nerd_chars[1]}%F{232}%K{170} VISUAL %k%F{170}${nerd_chars[2]}%b%f"
+vim_cmd_mode="%B%F{114}${nerd_chars[1]}%F{232}%K{114} NORMAL %k%F{114}${nerd_chars[3]}%b%f"
+vim_ins_mode="%B%F{039}${nerd_chars[1]}%F{232}%K{039} INSERT %k%F{039}${nerd_chars[3]}%b%f"
+vim_vis_mode="%B%F{232}${nerd_chars[1]}%F{232}%K{170} VISUAL %k%F{170}${nerd_chars[3]}%b%f"
 
 
 vim_mode=$vim_ins_mode
@@ -52,7 +55,7 @@ zle -N zle-line-finish
 
 # Check the UID
 if [[ $UID -ne 0 ]]; then  # normal user
-	PR_USER="%F{022}${nerd_chars[3]}%F{232}%K{022} %n "  # HOST corrects colors
+	PR_USER="%F{022}${nerd_chars[6]}%F{232}%K{022} %n "  # HOST corrects colors
 	PR_USER_OP='%F{green}%#%f'
 	PR_PROMPT='${vim_mode} '
 else  # root
@@ -64,14 +67,14 @@ fi
 
 # Check if we are on SSH or not
 if [[ -n "$SSH_CLIENT"  ||  -n "$SSH2_CLIENT" ]]; then  # SSH
-	PR_GCLOUD_PROJ="%F{036}%K{022}${nerd_chars[3]}%F{032}%K{036} $ZSH_GCLOUD_PROMPT "
-	PR_HOST="%F{032}${nerd_chars[3]}%F{232}%K{032} %M %k%F{032}${nerd_chars[4]}%k%f"
+	PR_GCLOUD_PROJ="%F{036}%K{022}${nerd_chars[6]}%F{032}%K{036} $ZSH_GCLOUD_PROMPT "
+	PR_HOST="%F{032}${nerd_chars[6]}%F{232}%K{032} %M %k%F{032}${nerd_chars[2]}%k%f"
 else  # no SSH
-	PR_GCLOUD_PROJ="%F{107}%K{022}${nerd_chars[3]}%F{232}%K{107} $ZSH_GCLOUD_PROMPT "
-	PR_HOST="%F{148}${nerd_chars[3]}%F{232}%K{148} %M %k%F{148}${nerd_chars[4]}%k%f"
+	PR_GCLOUD_PROJ="%F{107}%K{022}${nerd_chars[6]}%F{232}%K{107} $ZSH_GCLOUD_PROMPT "
+	PR_HOST="%F{148}${nerd_chars[6]}%F{232}%K{148} %M %k%F{148}${nerd_chars[2]}%k%f"
 fi
 
-local display_time="%F{204}${nerd_chars[1]}%F{232}%K{204}%*%B%F{204}%K{170}${nerd_chars[2]} %b%k%f"
+local display_time="%F{204}${nerd_chars[1]}%F{232}%K{204}%*%B%F{204}%K{170}${nerd_chars[3]} %b%k%f"
 
 # Directory expansion for showing only the last 2 directories, if too long
 local current_dir='%(4~|%-1~/…/%2~|%3~)'
@@ -79,12 +82,12 @@ local current_dir='%(4~|%-1~/…/%2~|%3~)'
 #local current_dir="%50<…<%~%<<"
 
 # Version control colorized
-local formatted_vcs_info="%F{232}%K{170}\${vcs_info_msg_0_}%B%F{170}%K{097}${nerd_chars[2]}%b%k%f"
+local formatted_vcs_info="%F{232}%K{170}\${vcs_info_msg_0_}%B%F{170}%K{097}${nerd_chars[3]}%b%k%f"
 
-local formatted_current_dir="%B%F{232}%K{097} ${current_dir}%k%F{097}${nerd_chars[2]}%b%k%f"
+local formatted_current_dir="%B%F{232}%K{097} ${current_dir}%k%F{097}${nerd_chars[3]}%b%k%f"
 
 # aka exit code, U+F112
-local return_code="%(?..%F{088}${nerd_chars[3]}%F{232}%K{088} %? %{[00;38;5;088m%}${nerd_chars[4]}%k%f)"
+local return_code="%(?..%F{088}${nerd_chars[6]}%F{232}%K{088} %? %{[00;38;5;088m%}${nerd_chars[2]}%k%f)"
 local user_host="${PR_USER}${PR_GCLOUD_PROJ}${PR_HOST}"
 
 # TWO LINE PROMPT
